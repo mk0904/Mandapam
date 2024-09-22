4858bc8c02ffcba2a1cbf3fa6d79b97b00152f08
mk0904 <mk10092004@gmail.com>
2024-09-22 10:39:46 +0530

    menubar responsiveness added

--- src/components/menubar/menubar.jsx
+++ src/components/menubar/menubar.jsx
@@ -1,16 +1,28 @@
-import { Calendar } from 'primereact/calendar';
-import Hero from '../hero/Hero';
+import React, { useState } from 'react';
 import './menubar.css';
-import { NavLink , Routes, Route} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Menubar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className='br-header'>
       <nav className="navbar">
         <div className="navbar-left">
           <NavLink to="/" className="main-logo">mandapam</NavLink>
         </div>
-        <div className="navbar-center">
+        
+        {/* Toggle button for smaller screens */}
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        
+        {/* Menu items (show/hide based on menuOpen state) */}
+        <div className={`navbar-center ${menuOpen ? 'open' : ''}`}>
           <ul>
             <li><NavLink to="/resorts">Resorts</NavLink></li>
             <li><NavLink to="/vendors">Vendors</NavLink></li>
@@ -18,14 +30,14 @@ function Menubar() {
             <li><NavLink to="/rivaaya">Rivaaya</NavLink></li>
           </ul>
         </div>
-        <div className="navbar-right">
+        
+        <div className={`navbar-right ${menuOpen ? 'open' : ''}`}>
           <ul>
-            <li><NavLink className='mandapamfc' to="/mfc">mandapam <span className='s1'>for <span className='s2'>corporates</span></span> </NavLink></li>
+            <li><NavLink className='mandapamfc' to="/mfc">mandapam <span className='s1'>for <span className='s2'>corporates</span></span></NavLink></li>
             <li><NavLink to="/login">Login</NavLink></li>
           </ul>
         </div>
       </nav>
-      
     </div>
   );
 }
